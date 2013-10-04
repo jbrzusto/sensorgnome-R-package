31057c5e607d2d49574f58ca37d052682da592ff
john <jbrzusto@fastmail.fm>
2013-10-04 17:43:37 +0000

    add -j flag to make invocation for faster multicore compilation

--- rebuild_exec.sh
+++ rebuild_exec.sh
@@ -5,20 +5,20 @@
 ./update_src.sh
 
 cd src/filter_tags
-make -f Makefile_xmingw clean
-make -f Makefile_xmingw
+make -j -f Makefile_xmingw clean
+make -j -f Makefile_xmingw
 /bin/cp -f filter_tags.exe ../../inst/bin
-make -f Makefile clean
-make -f Makefile
+make -j -f Makefile clean
+make -j -f Makefile
 /bin/cp -f filter_tags ../../inst/bin
 
 cd ../..
 
 cd src/find_tags
-make -f Makefile_xmingw clean
-make -f Makefile_xmingw
+make -j -f Makefile_xmingw clean
+make -j -f Makefile_xmingw
 /bin/cp -f find_tags.exe find_tags_unifile.exe ../../inst/bin
-make -f Makefile clean
-make -f Makefile
+make -j -f Makefile clean
+make -j -f Makefile
 /bin/cp -f find_tags find_tags_unifile ../../inst/bin
 cd ../..
