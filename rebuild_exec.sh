1faa19d1f8af42ba031220bf707f42a62e1250bc
john brzustowski <jbrzusto@fastmail.fm>
2013-10-10 16:53:38 +0000

    don't install crufty find_tags

--- rebuild_exec.sh
+++ rebuild_exec.sh
@@ -17,8 +17,8 @@ cd ../..
 cd src/find_tags
 make -j -f Makefile_xmingw clean
 make -j -f Makefile_xmingw
-/bin/cp -f find_tags.exe find_tags_unifile.exe ../../inst/bin
+/bin/cp -f find_tags_unifile.exe ../../inst/bin
 make -j -f Makefile clean
 make -j -f Makefile
-/bin/cp -f find_tags find_tags_unifile ../../inst/bin
+/bin/cp -f find_tags_unifile ../../inst/bin
 cd ../..
