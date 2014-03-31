17bfdd04fe6147a8a83c46a6a4cab29b71063925
john brzustowski <jbrzusto@fastmail.fm>
2014-03-31 14:22:18 +0000

    add readGPX to exports; extend to read Tracks, not just Waypoints

--- R/readGPX.R
+++ R/readGPX.R
@@ -1,26 +1,44 @@
-## read a WPX file and return waypoints as a data frame with these columns
+## read a GPX file and return trackpoints and waypoints as a data
+## frame with these columns
 ##
 ## ts: timestamp  (GMT)
 ## lat: latitude  (degrees N)
 ## lon: longitude (degrees E)
 ## ele: elevation (metres ASL)
+## lab: character: 'wpt' or the name of the track to which the point belongs
 
-readWPX = function(file) {
-  root = xmlRoot(xmlTreeParse(file))
-  n = sum(names(root) == "wpt")
-  rv = data.frame(ts = numeric(n), lat = numeric(n), lon = numeric(n), ele = numeric(n))
+readGPX = function(file) {
+  ns = c(d="http://www.topografix.com/GPX/1/1")
+  doc = xmlParse(file)
+
+  ## count waypoints and trackpoints
+  n = length(getNodeSet(doc, "//d:trkpt|//d:wpt", namespaces = ns))
+
+  ## create return dataframe
+  rv = data.frame(ts = numeric(n), lat = numeric(n), lon = numeric(n), ele = numeric(n), lab=I(character(n)))
+  
+  lab = "" ## filled in by <trk> element
   i = 1
-  xmlApply(root,
+  
+  xpathApply(doc,
+             "//d:trkpt|//d:wpt|//d:trk",
+             
            function(n) {
-             if (xmlName(n) == "wpt") {
+             nm = xmlName(n)
+             if (nm == "trk") {
+               lab <<- xmlValue(xmlChildren(n)[["name"]])
+             } else if (nm == "wpt" || nm == "trkpt") {
                rv$ts[i] <<- as.POSIXct(xmlValue(xmlChildren(n)[["time"]]), format="%Y-%m-%dT%H:%M:%S")
                attr = as.numeric(xmlAttrs(n))
                rv$lat[i] <<- attr[1]
                rv$lon[i] <<- attr[2]
                rv$ele[i] <<- as.numeric(xmlValue(xmlChildren(n)[["ele"]]))
+               rv$lab[i] <<- if (nm == "wpt") "wpt" else lab
                i <<- i + 1
              }
-           })
+           }
+             , namespaces = ns
+           )
   class(rv$ts) = c("POSIXt", "POSIXct")
   return(rv)
 }
