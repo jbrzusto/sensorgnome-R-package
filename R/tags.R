599626107dc4dab12ed40e83775449534979e42b
john brzustowski <jbrzusto@fastmail.fm>
2014-10-01 19:20:33 +0000

    new plotGlobalTags() function to give a summary of all a project's tag detections from all sites

--- R/tags.R
+++ R/tags.R
@@ -1,8 +1,12 @@
-tags = function(where, year = year(Sys.date() - 120 * 24 * 3600), filter = "and ((freqsd is null or freqsd < 0.1) and runLen > 2)") {
+tags = function(where, year = year(Sys.date() - 120 * 24 * 3600), filter = "and ((freqsd is null or freqsd < 0.1) and runLen > 2)", dbFile=NULL) {
   if (missing(where))
-    stop("Need to specify a subset to select, e.g. proj=='Taylr' and sp=='IPSP'")
+    stop("Need to specify a subset to select, e.g. \"proj='Taylr' and sp='IPSP'\"")
   require(RSQLite)
-  con = dbConnect("SQLite", sprintf("/SG/%d_alltags.sqlite", year))
+  if (is.null(dbFile))
+      dbFile = sprintf("/SG/%d_alltags.sqlite", year)
+  if (!file.exists(dbFile))
+      stop("non-existent tag detections database file: ", dbFile)
+  con = dbConnect("SQLite", dbFile)
   x = dbGetQuery(con, sprintf("select * from tags where %s %s", where, filter))
   class(x$ts) = c("POSIXt", "POSIXct")
   x$label = as.factor(x$label)
